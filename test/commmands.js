218e4271a2238bbd21466c3293135a1d178827ec
pvnguyen21@clc.fitus.edu.vn <126704701+nguyen30042003@users.noreply.github.com>
2024-08-29 21:25:02 +0700

    upload code smart contract place NFT

---
+++ test/commmands.js
@@ -0,0 +1,4 @@
+const instance = await NftMarket.deployed();
+
+instance.mintToken("https://apricot-additional-sheep-711.mypinata.cloud/ipfs/QmQjcm6WTKvX1Zc6u59Cc8D7pWCpq7Rfqw6JCPUDWv1n3X","500000000000000000", {value: "25000000000000000",from: accounts[0]})
+instance.mintToken("https://apricot-additional-sheep-711.mypinata.cloud/ipfs/QmZfkh9qxzbLYh5B3SMDUe9PGKxkYoSUAVfDkPjnqpr6QF","300000000000000000", {value: "25000000000000000",from: accounts[0]})
